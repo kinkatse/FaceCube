d897e4810a383f726c99914a4732c3babfb813d5
kakintse <kinkatse@mail.adelphi.edu>
2023-10-01 00:53:27 -0400

    Add LoginForm Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import { Route, Switch } from 'react-router-dom'
+import LoginForm from "./components/LoginForm";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>Hello from App</h1>
+        </Route>
+        <Route path="/login">
+          <LoginForm/>
+        </Route>
+      </Switch>
+    </main>
   );
 }
 
