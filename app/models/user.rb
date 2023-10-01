500504fc83c6f0c639fca2e7ef95fd46eb325f07
kakintse <kinkatse@mail.adelphi.edu>
2023-09-30 20:16:38 -0400

    Complete User Model for User Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,25 @@ class User < ApplicationRecord
 
   before_validation :ensure_session_token
 
+  def self.find_by_credentials(credential, password)
+    # If credential is of the URI::MailTo::EMAIL_REGEXP format,
+    # then we set field to be :email, if not, then :username
+    field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+    # This is the way to preserve that the field is the symbol of :email
+    # or :username and point it to the credential variable reference
+    # which actually holds the value of email or name
+    user = User.find_by(field => credential)
+    # Syntactic sugar for user && user.authenticate(password)
+    # the authenticate method comes from the has_secure_password macro that does
+    # all the password=, is_password, and password validations for us
+    user&.authenticate(password)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
   private
 
   def generate_unique_session_token
