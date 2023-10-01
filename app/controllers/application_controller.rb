05eb2509c896f8205014a64cf4e3349b89e8ae0a
kakintse <kinkatse@mail.adelphi.edu>
2023-09-30 20:44:14 -0400

    Add methods in Application Controller for User Auth and tested them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,43 @@
 class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
 
-    protect_from_forgery with: :exception
+    # protect_from_forgery with: :exception
     before_action :snake_case_params
-    before_action :attach_authenticity_token
+    # before_action :attach_authenticity_token
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'username', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+    end
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :Unauthorized
+        end
+    end
 
     private
     def snake_case_params
