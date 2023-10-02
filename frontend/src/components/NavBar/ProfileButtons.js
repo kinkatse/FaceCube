34ded5a1ba09c5dd1eb7657a82f2d5285bf5d36d
kakintse <kinkatse@mail.adelphi.edu>
2023-10-01 22:56:59 -0400

    Add Style to Profile Dropdown's Header

--- frontend/src/components/NavBar/ProfileButtons.js
+++ frontend/src/components/NavBar/ProfileButtons.js
@@ -28,21 +28,37 @@ const ProfileButtons = ({ user }) => {
     dispatch(sessionActions.logout());
   };
 
+  let profileHeader = (
+    <header className="profile-dropdown-header">
+      <i className="fa-solid fa-user user-pic"/>
+      <div className="profile-dropdown-details">
+        {/* <h1>{user.firstName}</h1> */}
+        <h1>Kin Ka Tse</h1>
+        <h2>{user.username}</h2>
+        <h2>{user.email}</h2>
+        <button>Edit User Info</button>
+      </div>
+    </header>
+  )
+
+  let userOptions = (
+    <section>
+      <NavLink exact to={`/channel/${user.id}`}>Channel</NavLink>
+      <NavLink exact to={`/studio`}>Studio</NavLink>
+      <button onClick={logout}>Sign Out</button>
+    </section>
+  )
+
   return (
     <>
       <button onClick={openMenu} className="dropdown-button">
         <i className="fa-solid fa-user"/>
       </button>
       {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <NavLink exact to={`/channel/${user.id}`}>Channel</NavLink>
-          <NavLink exact to={`/studio`}>Studio</NavLink>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
+        <div className="profile-dropdown">
+          {profileHeader}
+          {userOptions}
+        </div>
       )}
     </>
   );
