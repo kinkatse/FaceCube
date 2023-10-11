8cf886b1740924ae0a168a03f19cb714f2570c3e
kakintse <kinkatse@mail.adelphi.edu>
2023-10-11 15:33:53 -0400

    Fix image sizes & Style video index item info on home page

--- frontend/src/components/VideoIndex/VideoIndexItem.js
+++ frontend/src/components/VideoIndex/VideoIndexItem.js
@@ -10,16 +10,21 @@ const VideoIndexItem = ({ video, videoId }) => {
             <img className="video-index-item-image" src={`${video.thumbnail}`} />
           </NavLink>
           <div className={`${indexClass}-item-details`}>
-            <NavLink exact to={`/video/${video.id}`}>
-              <h1 className={`${indexClass}-item-title`}>{video.title}</h1>
-            </NavLink>
-            <NavLink exact to={`/channel/${video.userId}`}>
-              <h1 className={`${indexClass}-item-username`}>{video.username}</h1>
-            </NavLink>
-            <div className={`${indexClass}-other-details`}>
-              <h1 className={`${indexClass}-item-views`}>{video.views} views</h1>
-              <h1 className={`${indexClass}-item-date`}>{video.createdAt} ago</h1>
-            </div>
+              <NavLink exact to={`/channel/${video.userId}`} className={`video-${indexClass}-icon`}>
+                <i className="fa-solid fa-user"/>
+              </NavLink>
+              <div>
+                <NavLink exact to={`/video/${video.id}`}>
+                  <h1 className={`${indexClass}-item-title`}>{video.title}</h1>
+                </NavLink>
+                <NavLink exact to={`/channel/${video.userId}`}>
+                  <h1 className={`${indexClass}-item-username`}>{video.username}</h1>
+                </NavLink>
+                <div className={`${indexClass}-other-details`}>
+                  <h1 className={`${indexClass}-item-views`}>{video.views} views</h1>
+                  <h1 className={`${indexClass}-item-date`}>{video.createdAt} ago</h1>
+                </div>
+              </div>
           </div>
         </li>
     )
