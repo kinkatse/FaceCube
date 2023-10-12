da5d223b8ecad12dc4869cedd9db7bc4e71e8ade
kakintse <kinkatse@mail.adelphi.edu>
2023-10-11 23:18:57 -0400

    Add closing options for mini player & Include some styling

--- frontend/src/components/MiniPlayer/index.js
+++ frontend/src/components/MiniPlayer/index.js
@@ -1,26 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { openMiniPlayer, closeMiniPlayer } from "../../store/miniPlayer";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import './MiniPlayer.css'
 import VideoJS from "../VideoPlayer/VideoJS";
 
 const MiniPlayer = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
     const videoId = useSelector(state => state.ui.mini.videoId);
-    const video = useSelector(state => state.entities.videos[videoId])
+    const video = useSelector(state => state.entities.videos[videoId]);
 
-    const handleOpenMini = (videoId) => {
-      dispatch(openMiniPlayer(videoId));
-    }
     const handleCloseMini = () => {
       dispatch(closeMiniPlayer());
     }
 
-    // let iconButton = (
-    //   <button onClick={handleCloseMini} className="mini-close">
-    //     <i className="fa-solid fa-xmark"/>
-    //   </button>
-    // )
+    const handleClickExpand = () => {
+      dispatch(closeMiniPlayer());
+      history.push(`/video/${videoId}`);
+      return;
+    }
 
     if (!videoId) return null;
 
@@ -38,8 +36,14 @@ const MiniPlayer = () => {
     return (
       <div className="mini-player">
         <div className='videojs-container'>
+          <button onClick={handleClickExpand} className="mini-expand">
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </button>
           <VideoJS options={videoJsOptions} />
           <div className='behind-player-background'></div>
+          <button onClick={handleCloseMini} className="mini-close">
+            <i className="fa-solid fa-xmark"/>
+          </button>
         </div>
       </div>
     )
